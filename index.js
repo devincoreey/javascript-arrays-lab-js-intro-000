4d6277ff25babe831b6ecb95832326c8b89ef222
devin williams <williams.devin702@gmail.com>
2020-06-11 00:15:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 
 // Add your functions and code here
 function destructivelyAppendKitten(name){
-  kittens2
+  kittens2= ['floyd'];
+  kittens3= kittens.concat(kittens2);
 }
