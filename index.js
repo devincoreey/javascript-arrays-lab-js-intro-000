5aa82070a664fe8a2e9a4192701a290b1dffa7d3
devin williams <williams.devin702@gmail.com>
2020-06-11 00:24:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,4 +7,6 @@ function destructivelyAppendKitten(name){
 function destructivelyPrependKitten(name){
   kittens.unshift('Bob')
 }
-function 
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+}
